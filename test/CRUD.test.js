0f693f9ab3cc6682e514829d8a4a543337a3b172
CCharlieLi <ccharlieli@live.com>
2015-10-18 14:06:51 +0800

    update tests into 'promise'

--- test/CRUD.test.js
+++ test/CRUD.test.js
@@ -7,42 +7,46 @@ describe('json-parsing', function() {
     before(function() {
         db = getDataSource();
         Person = db.createModel('person', {id: Number, name: String, age: Number});
+        persons = [
+          {
+            id: 1,
+            name: 'Mary',
+            age: 34
+          },
+          {
+            id: 2,
+            name: 'Charlie',
+            age: 24
+          }
+        ];
+        config = {
+          ttl: 3  //seconds
+        };
     });
 
     it('create should create new item', function(done) {
-      Person.create({
-        id: 2,
-        name: 'Mary',
-        age: 34
-      }, function(err, res) {
-        if(err) console.log(err);
-        Person.findById('2',function(err, res) {
-          res.id.should.equal(2);
-          res.name.should.equal('Mary');
-          res.age.should.equal(34);
-          done(err, res);
-        })
+      return Person.create(persons[0]).then(function(res) {
+        res.name.should.eql('Mary');
+        done();
+      }).catch(function(err){
+        done(err);
       });
     });
 
     it('create should create new item with mixin EXPIRE in 3s', function(done) {
-      config = {
-        ttl: 3  //seconds
-      };
       mixin(Person,config);
-
-      Person.create({
-        id: 3,
-        name: 'Charlie',
-        age: 24
-      }, function(err, res) {
-        if(err) console.log(err);
+      return Person.create(persons[1]).then(function(res){
+        res.name.should.eql('Charlie');
         setTimeout(function(){
-          Person.findById('3',function(err, res) {
+          return Person.findById('3').then(function(res){
             should.not.exist(res.id);
-            done(err, res);
+            done();
+          }).catch(function(err){
+            done(err);
           });
         },config.ttl*1000);
+      }).catch(function(err){
+        done(err);
       });
     });
 
