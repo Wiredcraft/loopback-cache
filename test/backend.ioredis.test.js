e4daf85f9c058753a24c07875c0ae415db3951a2
CCharlieLi <ccharlieli@live.com>
2016-04-19 12:07:45 +0800

    add support for IORedis

--- test/backend.ioredis.test.js
+++ test/backend.ioredis.test.js
@@ -2,33 +2,35 @@ var should = require('should');
 var mixin = require('../mixins/cacheModel');
 var DataSource = require('loopback-datasource-juggler').DataSource;
 
-describe('Redis backend', function() {
+describe('IORedis backend', function () {
   var db;
   var Person;
   var id;
   var connector;
 
-  before(function() {
-    db = new DataSource(require('loopback-connector-redis'));
+  before(function () {
+    db = new DataSource(require('loopback-connector-ioredis'));
     connector = db.connector;
     Person = db.createModel('person', {
       id: String,
       name: String
     });
     mixin(Person, {
-      backend: 'redis',
+      backend: 'ioredis',
       ttl: 2 //s
     });
   });
 
   after(function (done) {
-    connector.client.flushall(done);
+    connector.connect().call('flushall').then(function () {
+      done();
+    }, done);
   });
 
   it('can create a new item', function (done) {
     return Person.create({
       name: 'Lorem'
-    }).then(function(person) {
+    }).then(function (person) {
       person.should.be.Object();
       person.name.should.equal('Lorem');
       id = person.id;
@@ -36,16 +38,16 @@ describe('Redis backend', function() {
     }).catch(done);
   });
 
-  it('can load the item', function(done) {
-    Person.findById(id).then(function(person) {
+  it('can load the item', function (done) {
+    Person.findById(id).then(function (person) {
       person.should.be.Object();
       person.name.should.equal('Lorem');
       done();
     }).catch(done);
   });
 
-  it('cannot load something not there', function(done) {
-    Person.findById(999).then(function(person) {
+  it('cannot load something not there', function (done) {
+    Person.findById(999).then(function (person) {
       person.should.be.Object();
       should(person.id).be.undefined();
       should(person.name).be.undefined();
@@ -53,9 +55,9 @@ describe('Redis backend', function() {
     }).catch(done);
   });
 
-  it('cannot load the item after 3 seconds', function(done) {
-    setTimeout(function() {
-      Person.findById('lorem').then(function(person) {
+  it('cannot load the item after 3 seconds', function (done) {
+    setTimeout(function () {
+      Person.findById('lorem').then(function (person) {
         person.should.be.Object();
         should(person.id).be.undefined();
         should(person.name).be.undefined();
